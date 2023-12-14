33f88095c6f87bf46d82f4f51dc2472d98d1e534
Kelechi Duru <kboss16058@gmail.com>
2023-12-14 18:21:04 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: kboss
+ *
+ * Created on December 14, 2023, 10:16 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
